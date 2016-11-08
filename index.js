554b4cea843a3fc131eadb475a5de847e94d0bc6
calvinvette <calvinvette@gmail.com>
2016-11-08 16:55:16 -0500

    CustomerTable as a SPA page

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ function updateStatus(message) {
   $("#myStatus").html(message);
 }
 
+function jump(link) {
+  var href = $(link).attr("href");
+  //console.log(href);
+  // #/CustomerTable
+  var afterFirstSlash = href.indexOf("/") + 1;
+  var baseName = href.substring(afterFirstSlash); // From afterFirstSlash to end of array
+  $.get(baseName + ".html", function(theFile) {
+    $("#content").html(theFile);
+  });
+}
+
 $(document).ready(function() {
 
-  $("#btnSubmit").click(function() {
+  $("#btnSubmit").click((evt) => {
     var customer = {
         firstName : $("#txtFirstName").val(),
         lastName : $("#txtLastName").val(),
